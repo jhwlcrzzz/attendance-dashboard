17db8135b4352f6a9f9eb254eede481618fdcf65
jhwlcrzzz <c.j.cruz1229@gmail.com>
2025-04-29 12:11:37 +0800

    Update mini-thesis-dashboard.py

--- mini-thesis-dashboard.py
+++ mini-thesis-dashboard.py
@@ -12,8 +12,8 @@ from streamlit_autorefresh import st_autorefresh # Keep if using this method
 
 
 # --- Configuration ---
-DATA_CACHE_TTL_SECONDS = 7 # Fetch data max every 15 seconds (Adjust as needed)
-APP_REFRESH_INTERVAL_SECONDS = 16 # Rerun UI every 5 seconds (Requires st_autorefresh or similar)
+DATA_CACHE_TTL_SECONDS = 6 # Fetch data max every 15 seconds (Adjust as needed)
+APP_REFRESH_INTERVAL_SECONDS = 6 # Rerun UI every 5 seconds (Requires st_autorefresh or similar)
 
 logo_path = os.path.join("sd", f"logo.jpg")
 st.logo(logo_path, size="large")
